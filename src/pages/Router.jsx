b08b9b72c9e301d94207e37a1f34856a263b30f4
anis <medanis.lakhdar@gmail.com>
2023-06-09 14:43:52 +0200

    adding router and all static layout components nav footer 404 and adding template user and signin

--- src/pages/Router.jsx
+++ src/pages/Router.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Home/Home';
+import Footer from '../components/Footer/Footer';
+import Nav from '../components/Nav/Nav';
+import SignIn from './Sign-in/SignIn';
+import User from './User/User';
+import Error from './404/Error';
 
 export default function Router() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/user-profile" element={<User />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
